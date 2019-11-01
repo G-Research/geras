de09d5e2da326c1d7f0f2b7235c809f2b659a358
David Leadbeater <David.Leadbeater@gresearch.co.uk>
2019-11-01 17:49:28 +0000

    Fixes

--- test/test-query.sh
+++ test/test-query.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
-docker-compose up -d --abort-on-container-exit --force-recreate
+apt-get install -y jq
+
+docker-compose up -d --force-recreate
 
 i=10
 while [[ $i -gt 0 ]]; do
-  if curl http://localhost:10902 | grep -q healthy; then
+  if curl http://localhost:10902/-/healthy | grep -q healthy; then
     break
   fi
   i=$[$i - 1]
   sleep 10
 done
 
-curl "http://localhost:10902/api/v1/query?query=test%3Aa%3A5&dedup=true&partial_response=true&time=1572629812.291&_=1572629811861"
+set -e
+
+# We just check we get some JSON back for now...
+curl "http://localhost:10902/api/v1/query?query=test%3Aa%3A5&dedup=true&partial_response=true&time=1572629812.291&_=1572629811861" | jq .
